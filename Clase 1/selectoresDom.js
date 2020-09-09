e2df09b0bcd61e5c438b1ae5c38a76ebb6c35b66
Luis Briceño <lalbricenov@gmail.com>
2020-09-09 14:17:25 -0500

    Js added

---
+++ Clase 1/selectoresDom.js
@@ -0,0 +1,5 @@
+// querySelector sirve para seleccionar elementos del DOM usando selectores de css
+let lista = document.querySelector("#miLista");
+console.log(lista);
+// lista.style.display = "none";
+lista.style.backgroundColor = "purple";
